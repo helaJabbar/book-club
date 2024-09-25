8beb385f62f87eaa3a8bc546cc9fbd401121d4c6
helaJabbar <eljabbarhela@yahoo.fr>
2024-09-25 15:14:57 +0100

    Version finale

--- client/src/components/Accueil.js
+++ client/src/components/Accueil.js
@@ -28,7 +28,7 @@ const Accueil = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
           dots: true
